4206f8c15547ce8fcc65917f1b58113b575de004
Xatoo <78871926+Xatoo@users.noreply.github.com>
2023-05-27 23:54:21 +0200

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -1,9 +1,9 @@
 int emptyFile(const char *nameFile);
-void saveGrid(char **grille, int M, int N, const char *nameFile);
-void saveData(const char *nameFile, int M, int N, unsigned long tps, int score,
+void saveGrid(char **grid, int M, int N, const char *nameFile);
+void saveData(const char *nameFile, int M, int N, unsigned long tms, int score,
               int nbSymb);
 void saveHighscores(const char *nameFile, char *name, int score,
-                    unsigned long tps);
+                    unsigned long tms);
 void printHighscores(char const *nameFile);
-void loadData(const char *nameFile, unsigned long *tps, int *M, int *N,
+void loadData(const char *nameFile, unsigned long *tms, int *M, int *N,
               int *score, int *nbSymb);
