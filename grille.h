f12cd6517c96daf0a69b33bce8c57e1378698bc0
Xatoo <78871926+Xatoo@users.noreply.github.com>
2023-05-27 23:53:02 +0200

    Update grille.h

--- grille.h
+++ grille.h
@@ -1,6 +1,6 @@
-void printgrid(char **grille, int M, int N, int score, int num_symbole);
-void remplis(char **grille, int M, int N, int num_symboles);
-char **create_grid(int M, int N, int num_symboles);
-char **swap_symbols(char **grille, int x1, int y1, int x2, int y2);
-void gravity(char **grille, int M, int N);
-int checkwin(char **grille, int M, int N);
+void printgrid(char **grid, int M, int N, int score, int num_symbol);
+void remplis(char **grid, int M, int N, int num_symbols);
+char **create_grid(int M, int N, int num_symbols);
+char **swap_symbols(char **grid, int x1, int y1, int x2, int y2);
+void gravity(char **grid, int M, int N);
+int checkwin(char **grid, int M, int N);
