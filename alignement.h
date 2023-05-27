8695cc2bb3f2e28bb35ab612223dd28f9ddbdf2b
Xatoo <78871926+Xatoo@users.noreply.github.com>
2023-05-27 21:33:06 +0200

    Delete alignement.h

--- alignement.h
+++
@@ -1,6 +0,0 @@
-
-
-void horizontale(char **grille, int M, int N);
-void verticale(char **grille, int M, int N);
-void diagonale1(char **grille, int M, int N);
-void diagonale2(char **grille, int M, int N);
