13eb98853ea8b9800635d8ec0b363080a8e6efda
Xatoo <78871926+Xatoo@users.noreply.github.com>
2023-05-27 23:56:30 +0200

    Update alignement.h

--- alignement.h
+++ alignement.h
@@ -1,7 +1,7 @@
-int alignment(char **grille, int M, int N);
-void horizontal(char **grille, int M, int N);
-void vertical(char **grille, int M, int N);
-void diagonal1(char **grille, int M, int N);
-void diagonal2(char **grille, int M, int N);
-int alignment_possible(char **grille, int M, int N);
-void del_alignment(char **grille, int M, int N);
+int alignment(char **grid, int M, int N);
+void horizontal(char **grid, int M, int N);
+void vertical(char **grid, int M, int N);
+void diagonal1(char **grid, int M, int N);
+void diagonal2(char **grid, int M, int N);
+int alignment_possible(char **grid, int M, int N);
+void del_alignment(char **grid, int M, int N);
